576fc2d504d5397b486ea8c1ee1700be1956a957
Guygool <64363416+Guygool@users.noreply.github.com>
2021-05-28 13:40:28 +0300

    search by name or partial name

--- dist/DB.sql
+++ dist/DB.sql
@@ -1,22 +1,20 @@
 
-create table games(
-    game_id int not null UNIQUE,
-    date date not null,
-    time time not null,
-    home_team int not null,
-    away_team int not null,
-    field varchar(30) not null,
-    home_goal int,
-    away_goal int,
-    event varchar(300),
-);
+SELECT name FROM  players where name like '%i%'
+-- create table players(
+--     name varchar(30) not null UNIQUE,
+--     id int 
+-- );
+-- insert into players( name, id)
+-- VALUES ('messi',NULL)
+-- insert into players( name, id)
+-- VALUES ('Cristiano Ronaldo',NULL)
 
-insert into games(game_id, date, time, home_team, away_team, field,home_goal, away_goal, event)
-VALUES( 1 , '2021-05-04' , ' 15:30:00' , 83 , 10086 , 'San Siro', 2 , 0 ,'good game');
-insert into games(game_id, date, time, home_team, away_team, field,home_goal, away_goal, event)
-VALUES( 2 , '2021-07-04' , ' 15:30:00' , 103 , 83 , 'San Siro', 0 , 1 ,'good game');
-insert into games(game_id, date, time, home_team, away_team, field,home_goal, away_goal, event)
-VALUES( 3 , '2021-08-04' , ' 15:30:00' , 103 , 10086 , 'San Siro', 2 , 0 ,'good game');
-insert into games(game_id, date, time, home_team, away_team, field,home_goal, away_goal, event)
-VALUES( 4 , '2021-08-05' , ' 15:30:00' , 83 , 10086 , 'San Siro', 2 , 0 ,'good game');
-select * from games
+-- insert into games(game_id, date, time, home_team, away_team, field,home_goal, away_goal, event)
+-- VALUES( 1 , '2021-05-04' , ' 15:30:00' , 83 , 10086 , 'San Siro', 2 , 0 ,'good game');
+-- insert into games(game_id, date, time, home_team, away_team, field,home_goal, away_goal, event)
+-- VALUES( 2 , '2021-07-04' , ' 15:30:00' , 103 , 83 , 'San Siro', 0 , 1 ,'good game');
+-- insert into games(game_id, date, time, home_team, away_team, field,home_goal, away_goal, event)
+-- VALUES( 3 , '2021-08-04' , ' 15:30:00' , 103 , 10086 , 'San Siro', 2 , 0 ,'good game');
+-- insert into games(game_id, date, time, home_team, away_team, field,home_goal, away_goal, event)
+-- VALUES( 4 , '2021-08-05' , ' 15:30:00' , 83 , 10086 , 'San Siro', 2 , 0 ,'good game');
+-- select * from games
--- project/routes/utils/players_utils.js
+++ project/routes/utils/players_utils.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const api_domain = "https://soccer.sportmonks.com/api/v2.0";
+const DButils = require('./DButils');
 // const TEAM_ID = "85";
 
 async function getPlayerIdsByTeam(team_id) {
@@ -32,6 +33,48 @@ async function getPlayersInfo(players_ids_list) {
   return extractRelevantPlayerData(players_info);
 }
 
+async function getPlayersInfoByName(players_names_list) {
+  let promises = [];
+  players_names_list.map((name) =>
+    promises.push(
+      axios.get(`${api_domain}/players/search/${name.name}`, {
+        params: {
+          api_token: process.env.api_token,
+          include: "team",
+        },
+      })
+    )
+  );
+  let players_info = await Promise.all(promises);
+  //return(players_info);
+ return extractRelevantPlayerDataByName(players_info[0].data.data);
+}
+
+function extractRelevantPlayerDataByName(players_info) {
+  console.log(players_info);
+  return players_info.map((player_info) => {
+    console.log(player_info)
+    const { fullname, image_path, position_id } = player_info;
+    if ('team' in player_info){
+      const { name } = player_info.team.data;
+      return {
+        name: fullname,
+        image: image_path,
+        position: position_id,
+        team_name: name ,
+      };
+    }
+    else{
+      return {
+        name: fullname,
+        image: image_path,
+        position: position_id,
+        team_name: 'No Team' ,
+      };
+    }
+  });
+}
+
 function extractRelevantPlayerData(players_info) {
   return players_info.map((player_info) => {
     const { fullname, image_path, position_id } = player_info.data.data;
@@ -44,6 +87,7 @@ function extractRelevantPlayerData(players_info) {
     };
   });
 }
+
 
 async function getPlayerFull(player_id){
   const player_full = await axios.get(`${api_domain}/players/${player_id}`, {
@@ -67,6 +111,14 @@ async function getPlayersByTeam(team_id) {
   return players_info;
 }
 
+async function searchPlayerInDB(name){
+  let player_names =( 
+    await DButils.execQuery(`select name from players where name LIKE '%${name}%'`
+  ));
+  return player_names;
+}
 exports.getPlayersByTeam = getPlayersByTeam;
 exports.getPlayersInfo = getPlayersInfo;
+exports.getPlayersInfoByName = getPlayersInfoByName;
 exports.getPlayerFull = getPlayerFull;
+exports.searchPlayerInDB = searchPlayerInDB;
