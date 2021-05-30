08a713e4ed64c28dd148bbb813ca8306b9f1c4b8
shiritab <69760048+shiritab@users.noreply.github.com>
2021-05-30 14:55:20 +0300

    bonus: add result to game - done.

--- project/main.js
+++ project/main.js
@@ -58,7 +58,7 @@ const users = require("./routes/users");
 const league = require("./routes/league");
 const teams = require("./routes/teams");
 const players = require("./routes/players");
-const coaches = require("./routes/coaches")
+const games = require("./routes/games");
 
 //#endregion
 
@@ -112,7 +112,7 @@ app.use("/users", users);
 app.use("/league", league);
 app.use("/teams", teams);
 app.use("/players", players);
-// app.use("/coaches", coaches)
+app.use("/games", games);
 app.use(auth);
 
 app.use(function (err, req, res, next) {
--- sql queries/games.sql
+++ sql queries/games.sql
@@ -1,19 +1,32 @@
-create table games(
-    game_id int not null UNIQUE,
-    date date not null,
-    time time not null,
-    home_team int not null,
-    away_team int not null,
-    field varchar(30) not null,
-    home_goal int,
-    away_goal int,
-    event varchar(300),
-);
+-- create table games(
+--     game_id int not null UNIQUE,
+--     date date not null,
+--     time time not null,
+--     home_team int not null,
+--     away_team int not null,
+--     field varchar(30) not null,
+--     home_goal int,
+--     away_goal int,
+--     event varchar(300),
+-- );
 
-insert into games (game_id, date, time, home_team, away_team, field, home_goal, away_goal, event)
-values (1, '2021-06-01', '15:30:00', 'Inter', 'Barcelona', 'Camp Nou', NULL, NULL, NULL);
+-- insert into games (game_id, date, time, home_team, away_team, field, home_goal, away_goal, event)
+-- values (1, '2021-06-01', '15:30:00', 'Inter', 'Barcelona', 'Camp Nou', NULL, NULL, NULL);
 
-insert into games (game_id, date, time, home_team, away_team, field, home_goal, away_goal, event)
-values (2, '2021-06-09', '15:30:00', 'Manchester United', 'Barcelona', 'Manchester', NULL, NULL, NULL);
+-- insert into games (game_id, date, time, home_team, away_team, field, home_goal, away_goal, event)
+-- values (2, '2021-06-09', '15:30:00', 'Manchester United', 'Barcelona', 'Manchester', NULL, NULL, NULL);
+
+-- insert into games(game_id, date, time, home_team, away_team, field, home_goal, away_goal, event)
+-- values (5, '2021-05-30', '15:00:00', 83, 103, 'Camp Nou', NULL, NULL, NULL);
+
+-- insert into games(game_id, date, time, home_team, away_team, field, home_goal, away_goal, event)
+-- values (6, '2021-05-30', '09:00:00', 103, 83, 'Camp Nou', 3, 1, 'bla');
 
 select * from games
+-- update games
+-- SET
+--     home_goal = NULL,
+--     away_goal = NULL
+-- WHERE
+--     game_id = 5;
+
